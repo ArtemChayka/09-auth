0f9b6c01b15d68581c7ccb771c020066b3899951
Artem Chaychenko <drchaychenko@gmail.com>
2025-09-16 20:50:19 +0300

    13

--- types/note.ts
+++ types/note.ts
@@ -2,11 +2,12 @@ export interface Note {
   id: string;
   title: string;
   content: string;
-  tag: NoteTag;
+  tags: string[];
   createdAt: string;
   updatedAt: string;
 }
 
-export type NoteTag = "Todo" | "Work" | "Personal" | "Meeting" | "Shopping";
-
-
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
